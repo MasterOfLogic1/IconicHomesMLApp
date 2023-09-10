8cd99c34bbd279f74d23cc7f977dbf7fa9ceab44
David Oku <84320460+MasterOfLogic1@users.noreply.github.com>
2023-09-10 17:20:11 +0100

    Add files via upload

--- pages/4_Team.py
+++ pages/4_Team.py
@@ -13,7 +13,7 @@ st.write("<b style='font-size: 20px;'>David Oku</b>", unsafe_allow_html=True)
 st.write('Student MSc. Data Science')
 st.write('Leeds Beckett University,England, United Kingdom.')
 st.markdown("""
-About : I am a passionate about software engineering with keen interest in data science ,automation , machine learning and game development. I keen in seeing technology being used to gain insights and solve real-world problems.
+About : I am passionate about software engineering with keen interest in data science ,automation , machine learning and game development. I keen in seeing technology being used to gain insights and solve real-world problems.
 Grab a cofee and connect with me on [LinkedIn](https://www.linkedin.com/in/masteroflogic/) for a tour around my globe🙃🙃.
 """)
 st.markdown("""[Personal Portfolio](https://masteroflogic1.github.io/myportfolio/)""")
@@ -34,7 +34,9 @@ try:
 except FileNotFoundError:
     st.write("Image not found")
 
-st.write("<b style='font-size: 20px;'>Shan-A-Khuda, Mohammad</b>", unsafe_allow_html=True)
+st.write("<b style='font-size: 20px;'>Dr. Shan-A-Khuda, Mohammad</b>", unsafe_allow_html=True)
 st.write('Professor & Project Supervisor')
 st.write('Leeds Beckett University,England, United Kingdom.')
+st.markdown("""[Linked In](https://www.linkedin.com/in/mohammad-shan-a-khuda-8961051bb/)""")
+
 
